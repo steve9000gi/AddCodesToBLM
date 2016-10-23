4c2bdba75eaac23713e38bb89b86514340dd938f
Steve Chall <stevec@renci.org>
2016-10-23 13:58:05 -0400

    write blm with codes to .csv file

--- AddCodesToBLM.R
+++ AddCodesToBLM.R
@@ -25,9 +25,9 @@ outputCBLMFileName = args[8]
 print(paste("inputBLMFileName: ", inputBLMFileName, "; outputCBLMFileName: ",
       outputCBLMFileName))
 
-sortedList = fromJSON(inputSortFileName)
 options(stringsAsFactors=FALSE)
-blm = read.csv(inputBLMFileName, header = TRUE, sep = "\t", quote = "");
+sortedList = fromJSON(inputSortFileName)
+blm = read.csv(inputBLMFileName, header = FALSE, sep = "\t", quote = "");
 
 n = names(sortedList)
 revL = list() # reverse list: key is node name, value is associated code
@@ -35,23 +35,45 @@ for (i in 1:length(sortedList)) {
   code = n[i]
   l = as.list(sortedList[code][[1]])
   for (j in 1:length(l)) {
-    print(paste(code, ": ", l[[j]], "; ", class(l[[j]])), sep = "")
+#    print(paste(code, ": ", l[[j]], "; ", class(l[[j]])), sep = "")
     revL[l[[j]]] = code;
   }
 } 
 
 revNames = names(revL)
 for (i in 1:length(revL)) {
-  print(paste(revNames[i], ": ", revL[[i]]));
+#  print(paste(revNames[i], ": ", revL[[i]]));
   #print(revL[[i]]);
 }
 
-codeList = c()
-for (i in 1:length(blm[,4])) {
-  codeList[[i]] = revL[[blm[i,4]]]
-  print(paste(i, blm[i,4], class(blm[i,4])))
+codes = c("Code")
+for (i in 2:length(blm[,4])) {
+  codes[[i]] = revL[[blm[i,4]]]
+#  print(paste(i, blm[i,4], class(blm[i,4])))
 }
 
-for (i in 1:length(codeList)) {
-  print(codeList[[i]])
+nColsOut = dim(blm)[2] + 1
+nColsBefore = 4
+nColsAfter = dim(blm)[2] - nColsBefore
+codeColIx = nColsBefore + 1
+outList <- vector("list", nColsOut) 
+for (i in 1:nColsBefore) {
+  #outList[[i]] <- as.list(blm[,i])
+  outList[[i]] <- blm[,i]
 }
+#outList[[codeColIx]] = as.list(codes)
+outList[[codeColIx]] = codes
+for (i in (codeColIx + 1):nColsOut) {
+  #outList[[i]] <- as.list(blm[,(i - 1)])
+  outList[[i]] <- blm[,(i - 1)]
+}
+outList[[1]][[1]] = ""
+do.call(cbind, outList) 
+
+write.table(outList, 
+            file = outputCBLMFileName,
+            append = FALSE, 
+            quote = FALSE, 
+            sep = "\t", 
+            row.names = FALSE, 
+            col.names = FALSE)
